0d32cad27dcbcb69bf13a795e1ee62dd2191cd0b
Yulia ec <cyula90@gmail.com>
2023-04-13 16:14:31 +0700

    project init

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day; //день рождения
+    public int month;//месяц рождения
+    public int year;//год рождения
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; //поле для имени
+    public String passport; //поле для паспорта
+    public String patronymic; //поле для фамилии
+    public String phone; //поле для телефона
+    public String surname; //поле для фамилии
+    public boolean subscription; //получает ли смс о скидках
+
+    public FormDate birthday;//дата рождения
+}
